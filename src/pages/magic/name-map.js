c17618ae540328f0110a5eaedad933bc0fad1e25
Aleix Paradell <paradell.aleix@gmail.com>
2026-03-10 17:11:34 +0100

    remove other languages from src

--- src/pages/magic/name-map.js
+++ src/pages/magic/name-map.js
@@ -1,269 +1,163 @@
 export const nameMap = {
   greenskins: {
-    name_de: "Glitzakram",
     name_en: "Shiny Stuff",
-    name_fr: "Lé Trucs Ki Brill’",
   },
   "the-empire": {
-    name_de: "Magische Erbstücke",
     name_en: "Heirlooms of Magic",
-    name_fr: "Reliques Ensorcelées",
   },
   heirlooms: {
-    name_de: "Erbstücke",
     name_en: "Ancestral Heirlooms",
-    name_fr: "Héritages Ancestraux",
   },
   dwarfs: {
-    name_de: "Zwergenrunen",
     name_en: "Runic Items",
-    name_fr: "Objets Runiques",
   },
   general: {
-    name_de: "Gewöhnliche Magische Gegenstände",
     name_en: "Common Magic Items",
-    name_fr: "Objets Magiques Communs",
   },
   weapon: {
-    name_de: "Magische Waffen",
     name_en: "Magic Weapons",
-    name_fr: "Armes Magiques",
   },
   armor: {
-    name_de: "Magische Rüstungen",
     name_en: "Magic Armour",
-    name_fr: "Armures Magiques",
   },
   "armor-mages": {
-    name_de: "Magische Rüstungen",
     name_en: "Magic Armour",
-    name_fr: "Armures Magiques",
   },
   talisman: {
-    name_de: "Talismane",
     name_en: "Talismans",
-    name_fr: "Talismans",
   },
   banner: {
-    name_de: "Magische Standarten",
     name_en: "Magic Standards",
-    name_fr: "Bannières Magiques",
   },
   artifact: {
     // deprecated
-    name_de: "Arkane Artifakte",
     name_en: "Arcane Items",
-    name_fr: "Objets Cabalistiques",
   },
   "arcane-item": {
-    name_de: "Arkane Artifakte",
     name_en: "Arcane Items",
-    name_fr: "Objets Cabalistiques",
   },
   "enchanted-item": {
-    name_de: "Verzauberte Gegenstände",
     name_en: "Enchanted Items",
-    name_fr: "Objets Enchantés",
   },
   "weapon-runes": {
-    name_de: "Waffenrunen",
     name_en: "Weapon Runes",
-    name_fr: "Runes d’Arme",
   },
   "ranged-weapon-runes": {
-    name_de: "Ranged Weapon Runes",
     name_en: "Ranged Weapon Runes",
-    name_fr: "Runes d’Arme d’Ingénieur",
   },
   "armor-runes": {
-    name_de: "Rüstungsrunen",
     name_en: "Armour Runes",
-    name_fr: "Runes d’Armure",
   },
   "banner-runes": {
-    name_de: "Standartenrunen",
     name_en: "Banner Runes",
-    name_fr: "Runes de Bannière",
   },
   "talismanic-runes": {
-    name_de: "Talismanrunen",
     name_en: "Talismanic Runes",
-    name_fr: "Runes de Talisman",
   },
   "engineering-runes": {
-    name_de: "Maschinenrunen",
     name_en: "Engineering Runes",
-    name_fr: "Runes d’Ingénierie",
   },
   "runic-tattoos": {
-    name_de: "Runic Tattoos",
     name_en: "Runic Tattoos",
-    name_fr: "Tatouages Runiques",
   },
   triptych: {
-    name_de: "Triptych",
     name_en: "Triptych",
-    name_fr: "Triptyque",
   },
   triptychs: {
-    name_de: "Triptychs",
     name_en: "Triptychs",
-    name_fr: "Triptyques",
   },
   "kingdom-of-bretonnia": {
-    name_de: "Kingdom of Bretonnia",
     name_en: "Kingdom of Bretonnia",
-    name_fr: "Royaume de Bretonnie",
   },
   "chaos-mutations": {
-    name_de: "Chaos Mutations",
     name_en: "Chaos Mutations",
-    name_fr: "Mutations du Chaos",
   },
   "chaos-mutation": {
-    name_de: "Chaos Mutation",
     name_en: "Chaos Mutation",
-    name_fr: "Mutation du Chaos",
   },
   "chaos-mutation-character": {
-    name_de: "Chaos Mutation",
     name_en: "Chaos Mutation",
-    name_fr: "Mutation du Chaos",
   },
   "chaos-mutation-chieftain": {
-    name_de: "Chaos Mutation",
     name_en: "Chaos Mutation",
-    name_fr: "Mutation du Chaos",
   },
   "gifts-of-chaos": {
-    name_de: "Gifts of Chaos",
     name_en: "Gifts of Chaos",
-    name_fr: "Dons du Chaos",
   },
   "gift-of-chaos": {
-    name_de: "Gift of Chaos",
     name_en: "Gift of Chaos",
-    name_fr: "Don du Chaos",
   },
   "chaotic-traits": {
-    name_de: "Chaotic Traits",
     name_en: "Chaotic Traits",
-    name_fr: "Traits Chaotiques",
   },
   "chaotic-trait": {
-    name_de: "Chaotic Trait",
     name_en: "Chaotic Trait",
-    name_fr: "Trait Chaotique",
   },
   "forest-spites": {
-    name_de: "Forest Spites",
     name_en: "Forest Spites",
-    name_fr: "Fiel-follets des Forêts",
   },
   "forest-spite": {
-    name_de: "Forest Spite",
     name_en: "Forest Spite",
-    name_fr: "Fiel-follet des Forêts",
   },
   "beastmen-brayherds": {
-    name_de: "Beastmen Brayherds",
     name_en: "Beastmen Brayherds",
-    name_fr: "Braillehardes Homme-bêtes",
   },
   "orc-and-goblin-tribes": {
-    name_de: "Orc & Goblin Tribes",
     name_en: "Orc & Goblin Tribes",
-    name_fr: "Tribus des Orques & Gobelins",
   },
   "dwarfen-mountain-holds": {
-    name_de: "Dwarfen Mountain Holds",
     name_en: "Dwarfen Mountain Holds",
-    name_fr: "Forteresses Naines",
   },
   "expeditionary-force": {
-    name_de: "Expeditionary Force",
     name_en: "Expeditionary Force",
-    name_fr: "Corps Expéditionnaire",
   },
   "royal-clan": {
-    name_de: "Royal Clan",
     name_en: "Royal Clan",
-    name_fr: "Clan Royal",
   },
   "empire-of-man": {
-    name_de: "Empire of Man",
     name_en: "Empire of Man",
-    name_fr: "Empire de l’Homme",
   },
   "wood-elf-realms": {
-    name_de: "Wood Elf Realms",
     name_en: "Wood Elf Realms",
-    name_fr: "Royaumes Elfes Sylvains",
   },
   "high-elf-realms": {
-    name_de: "High Elf Realms",
     name_en: "High Elf Realms",
-    name_fr: "Royaumes Hauts Elfes",
   },
   "tomb-kings-of-khemri": {
-    name_de: "Tomb Kings of Khemri",
     name_en: "Tomb Kings of Khemri",
-    name_fr: "Rois des Tombes de Khemri",
   },
   "warriors-of-chaos": {
-    name_de: "Warriors of Chaos",
     name_en: "Warriors of Chaos",
-    name_fr: "Guerriers du Chaos",
   },
   "knightly-virtues": {
-    name_de: "Knightly Virtues",
     name_en: "Knightly Virtues",
-    name_fr: "Vertus Chevaleresques",
   },
   "repeated-knightly-virtues": {
-    name_de: "Repeated Knightly Virtues",
     name_en: "Repeated Knightly Virtues",
-    name_fr: "Vertu Chevaleresque",
   },
   "knightly-virtue": {
-    name_de: "Knightly Virtue",
     name_en: "Knightly Virtue",
-    name_fr: "Vertu Chevaleresque",
   },
   "repeated-knightly-virtue": {
-    name_de: "Knightly Virtue",
     name_en: "Repeated Knightly Virtue",
-    name_fr: "Vertu Chevaleresque",
   },
   "elven-honours": {
-    name_de: "Elven Honours",
     name_en: "Elven Honours",
-    name_fr: "Honneurs Elfiques",
   },
   "elven-honour": {
-    name_de: "Elven Honour",
     name_en: "Elven Honour",
-    name_fr: "Honneur Elfique",
   },
   none: {
-    name_de: "Keine",
     name_en: "None",
-    name_fr: "Aucun",
   },
   daemonology: {
-    name_de: "Dämonologie",
     name_en: "Daemonology",
-    name_fr: "Démonisme",
   },
   "dark-magic": {
-    name_de: "Schwarze Magie",
     name_en: "Dark Magic",
-    name_fr: "Magie Noire",
   },
   elementalism: {
-    name_de: "Elementarismus",
     name_en: "Elementalism",
-    name_fr: "Élémentalisme",
   },
   "lore-of-fire": {
     name_en: "Lore of Fire",
@@ -299,429 +193,259 @@ export const nameMap = {
     name_es: "Saber de los Cielos",
   },
   "battle-magic": {
-    name_de: "Kampfmagie",
     name_en: "Battle Magic",
-    name_fr: "Magie de Bataille",
   },
   "high-magic": {
-    name_de: "Weiße Magie",
     name_en: "High Magic",
-    name_fr: "Haute Magie",
   },
   illusion: {
-    name_de: "Illusion",
     name_en: "Illusion",
-    name_fr: "Illusionnisme",
   },
   necromancy: {
-    name_de: "Nekromantie",
     name_en: "Necromancy",
-    name_fr: "Nécromancie",
   },
   "waaagh-magic": {
-    name_de: "Waaagh! Magie",
     name_en: "Waaagh! Magic",
-    name_fr: "Magie Waaagh!",
   },
   "troll-magic": {
-    name_de: "Troll Magie",
     name_en: "Troll Magic",
-    name_fr: "Magie Troll",
   },
   shadowlands: {
-    name_de: "Shadowlands",
     name_en: "Shadowlands",
-    name_fr: "Terres des Ombres",
   },
   "errantry-crusades": {
-    name_de: "Errantry Crusades",
     name_en: "Errantry Crusades",
-    name_fr: "Croisades d’Errance",
   },
   "bretonnian-exiles": {
-    name_de: "Bretonnian Exiles",
     name_en: "Bretonnian Exiles",
-    name_fr: "Exilés Bretonniens",
   },
   "nehekharan-royal-hosts": {
-    name_de: "Nehekharan Royal Hosts",
     name_en: "Nehekharan Royal Hosts",
-    name_fr: "Osts Royaux de Nehekhara",
   },
   "mortuary-cults": {
-    name_de: "Mortuary Cults",
     name_en: "Mortuary Cults",
-    name_fr: "Cultes Mortuaires",
   },
   "nomadic-waaagh": {
-    name_de: "Nomadic Waaagh!",
     name_en: "Nomadic Waaagh!",
-    name_fr: "Waaagh! Nomade",
   },
   "troll-horde": {
-    name_de: "Troll Horde",
     name_en: "Troll Horde",
-    name_fr: "Horde de Trolls",
   },
   "wolves-of-the-sea": {
-    name_de: "Wolves of the Sea",
     name_en: "Wolves of the Sea",
-    name_fr: "Loups des Mers",
   },
   "heralds-of-darkness": {
-    name_de: "Heralds of Darkness",
     name_en: "Heralds of Darkness",
-    name_fr: "Hérauts des Ténèbres",
   },
   "chaos-dwarfs": {
-    name_de: "Chaos Dwarfs",
     name_en: "Chaos Dwarfs",
-    name_fr: "Nains du Chaos",
   },
   "cd-renegade": {
-    name_de: "Renegade",
     name_en: "Renegade",
-    name_fr: "Renegade",
   },
   "vampire-counts": {
-    name_de: "Vampirfürsten",
     name_en: "Vampire Counts",
-    name_fr: "Comtes Vampires",
   },
   "vc-renegade": {
-    name_de: "Renegade",
     name_en: "Renegade",
-    name_fr: "Renegade",
   },
   "vampiric-powers": {
-    name_de: "Vampirkräfte",
     name_en: "Vampiric Powers",
-    name_fr: "Pouvoirs Vampiriques",
   },
   "vampiric-power": {
-    name_de: "Vampirkraft",
     name_en: "Vampiric Power",
-    name_fr: "Pouvoir Vampirique",
   },
   "dark-elves": {
-    name_de: "Dark Elves",
     name_en: "Dark Elves",
-    name_fr: "Elfes Noirs",
   },
   "de-renegade": {
-    name_de: "Renegade",
     name_en: "Renegade",
-    name_fr: "Renegade",
   },
   skaven: {
-    name_de: "Skaven",
     name_en: "Skaven",
-    name_fr: "Skavens",
   },
   "sk-renegade": {
-    name_de: "Renegade",
     name_en: "Renegade",
-    name_fr: "Renegade",
   },
   "daemons-of-chaos": {
-    name_de: "Daemons of Chaos",
     name_en: "Daemons of Chaos",
-    name_fr: "Démons du Chaos",
   },
   "doc-renegade": {
-    name_de: "Renegade",
     name_en: "Renegade",
-    name_fr: "Renegade",
   },
   "daemonic-gifts-common": {
-    name_de: "Chaotic Gifts",
     name_en: "Chaotic Gifts",
-    name_fr: "Dons du Chaos",
   },
   "daemonic-gift-common": {
-    name_de: "Chaotic Gift",
     name_en: "Chaotic Gift",
-    name_fr: "Don du Chaos",
   },
   "daemonic-icons-common": {
-    name_de: "Chaotic Icons",
     name_en: "Chaotic Icons",
-    name_fr: "Icônes du Chaos",
   },
   "daemonic-icon-common": {
-    name_de: "Chaotic Icon",
     name_en: "Chaotic Icon",
-    name_fr: "Icône du Chaos",
   },
   "daemonic-gifts-khorne": {
-    name_de: "Gifts Of Khorne",
     name_en: "Gifts Of Khorne",
-    name_fr: "Dons de Khorne",
   },
   "daemonic-gift-khorne": {
-    name_de: "Gift Of Khorne",
     name_en: "Gift Of Khorne",
-    name_fr: "Don de Khorne",
   },
   "daemonic-icons-khorne": {
-    name_de: "Icons Of Khorne",
     name_en: "Icons Of Khorne",
-    name_fr: "Icônes de Khorne",
   },
   "daemonic-icon-khorne": {
-    name_de: "Icon Of Khorne",
     name_en: "Icon Of Khorne",
-    name_fr: "Icône de Khorne",
   },
   "daemonic-gifts-nurgle": {
-    name_de: "Gifts Of Nurgle",
     name_en: "Gifts Of Nurgle",
-    name_fr: "Dons de Nurgle",
   },
   "daemonic-gift-nurgle": {
-    name_de: "Gift Of Nurgle",
     name_en: "Gift Of Nurgle",
-    name_fr: "Don de Nurgle",
   },
   "daemonic-icons-nurgle": {
-    name_de: "Icons Of Nurgle",
     name_en: "Icons Of Nurgle",
-    name_fr: "Icônes de Nurgle",
   },
   "daemonic-icon-nurgle": {
-    name_de: "Icon Of Nurgle",
     name_en: "Icon Of Nurgle",
-    name_fr: "Icône de Nurgle",
   },
   "daemonic-gifts-slaanesh": {
-    name_de: "Gifts Of Slaanesh",
     name_en: "Gifts Of Slaanesh",
-    name_fr: "Dons de Slaanesh",
   },
   "daemonic-gift-slaanesh": {
-    name_de: "Gift Of Slaanesh",
     name_en: "Gift Of Slaanesh",
-    name_fr: "Don de Slaanesh",
   },
   "daemonic-icons-slaanesh": {
-    name_de: "Icons Of Slaanesh",
     name_en: "Icons Of Slaanesh",
-    name_fr: "Icônes de Slaanesh",
   },
   "daemonic-icon-slaanesh": {
-    name_de: "Icon Of Slaanesh",
     name_en: "Icon Of Slaanesh",
-    name_fr: "Icône de Slaanesh",
   },
   "daemonic-gifts-tzeentch": {
-    name_de: "Gifts Of Tzeentch",
     name_en: "Gifts Of Tzeentch",
-    name_fr: "Dons de Tzeentch",
   },
   "daemonic-gift-tzeentch": {
-    name_de: "Gift Of Tzeentch",
     name_en: "Gift Of Tzeentch",
-    name_fr: "Don de Tzeentch",
   },
   "daemonic-icons-tzeentch": {
-    name_de: "Icons Of Tzeentch",
     name_en: "Icons Of Tzeentch",
-    name_fr: "Icônes de Tzeentch",
   },
   "daemonic-icon-tzeentch": {
-    name_de: "Icon Of Tzeentch",
     name_en: "Icon Of Tzeentch",
-    name_fr: "Icône de Tzeentch",
   },
   "forbidden-poisons": {
-    name_de: "Forbidden Poisons",
     name_en: "Forbidden Poisons",
-    name_fr: "Poisons Interdits",
   },
   "gifts-of-khaine": {
-    name_de: "Gifts of Khaine",
     name_en: "Gifts of Khaine",
-    name_fr: "Dons de Khaine",
   },
   "forbidden-poison": {
-    name_de: "Forbidden Poison",
     name_en: "Forbidden Poison",
-    name_fr: "Poison Interdit",
   },
   "gift-of-khaine": {
-    name_de: "Gift of Khaine",
     name_en: "Gift of Khaine",
-    name_fr: "Don de Khaine",
   },
   "ogre-kingdoms": {
-    name_de: "Ogre Kingdoms",
     name_en: "Ogre Kingdoms",
-    name_fr: "Royaumes Ogres",
   },
   "ok-renegade": {
-    name_de: "Renegade",
     name_en: "Renegade",
-    name_fr: "Renegade",
   },
   "big-names": {
-    name_de: "Big Names",
     name_en: "Big Names",
-    name_fr: "Grands Noms",
   },
   "big-name": {
-    name_de: "Big Name",
     name_en: "Big Name",
-    name_fr: "Grand Nom",
   },
   "disciplines-old-ones": {
-    name_de: "Disciplines of the Old Ones",
     name_en: "Disciplines of the Old Ones",
-    name_fr: "Disciplines des Anciens",
   },
   "discipline-old-ones": {
-    name_de: "Discipline of the Old Ones",
     name_en: "Discipline of the Old Ones",
-    name_fr: "Discipline des Anciens",
   },
   lizardmen: {
-    name_de: "Lizardmen",
     name_en: "Lizardmen",
-    name_fr: "Hommes-Lézards",
   },
   "lm-renegade": {
-    name_de: "Renegade",
     name_en: "Renegade",
-    name_fr: "Renegade",
   },
   "city-state-of-nuln": {
-    name_de: "City-State of Nuln",
     name_en: "City-State of Nuln",
-    name_fr: "Cité-État de Nuln",
   },
   "knightly-order-panther": {
-    name_de: "Order of the Knights Panther",
     name_en: "Order of the Knights Panther",
-    name_fr: "Ordre des Chevaliers Panthères",
   },
   "knightly-order-white-wolf": {
-    name_de: "Order of the White Wolf",
     name_en: "Order of the White Wolf",
-    name_fr: "Ordre du Loup Blanc",
   },
   "knightly-order-blazing-sun": {
-    name_de: "Order of the Blazing Sun",
     name_en: "Order of the Blazing Sun",
-    name_fr: "Order du Soleil",
   },
   "knightly-order-morr": {
-    name_de: "Order of the Knights of Morr",
     name_en: "Order of the Knights of Morr",
-    name_fr: "Ordre des Chevaliers de Morr",
   },
   "knightly-order-fiery-heart": {
-    name_de: "Order of the Fiery Heart",
     name_en: "Order of the Fiery Heart",
-    name_fr: "Ordre du Cœur Ardent",
   },
   "the-chracian-warhost": {
-    name_de: "The Chracian Warhost",
     name_en: "The Chracian Warhost",
-    name_fr: "L’Ost de Chrace",
   },
   "sea-guard-garrison": {
-    name_de: "Sea Guard Garrison",
     name_en: "Sea Guard Garrison",
-    name_fr: "Garnison de la Garde Maritime",
   },
   "orions-wild-hunt": {
-    name_de: "Orion's Wild Hunt",
     name_en: "Orion's Wild Hunt",
-    name_fr: "Chasse Sauvage d’Orion",
   },
   "host-of-talsyn": {
-    name_de: "Host of Talsyn",
     name_en: "Host of Talsyn",
-    name_fr: "Ost de Talsyn",
   },
   "lore-of-the-wilds": {
-    name_de: "Lore of the Wilds",
     name_en: "Lore of the Wilds",
-    name_fr: "Domaine des Terres Sauvages",
   },
   kindreds: {
-    name_de: "Alter/Noble Kindreds",
     name_en: "Alter/Noble Kindreds",
-    name_fr: "Clans Changeformes/Nobles",
   },
   kindred: {
-    name_de: "Kindred",
     name_en: "Kindred",
-    name_fr: "Clan",
   },
   "minotaur-blood-herd": {
-    name_de: "Minotaur Blood Herd",
     name_en: "Minotaur Blood Herd",
-    name_fr: "Harde de Sang Minotaure",
   },
   "wild-herd": {
-    name_de: "Wild Herd",
     name_en: "Wild Herd",
-    name_fr: "Harde Sauvage",
   },
   "primal-magic": {
-    name_de: "Lore of Primal Magic",
     name_en: "Lore of Primal Magic",
-    name_fr: "Domaine de Magie Primordiale",
   },
   "grand-cathay": {
-    name_de: "Grand Cathay",
     name_en: "Grand Cathay",
-    name_fr: "Grand Cathay",
   },
   "lore-of-yang": {
-    name_de: "Lore of Yang",
     name_en: "Lore of Yang",
-    name_fr: "Domaine de Yang",
   },
   "lore-of-yin": {
-    name_de: "Lore of Yin",
     name_en: "Lore of Yin",
-    name_fr: "Domaine de Yin",
   },
   "jade-fleet": {
-    name_de: "Jade Fleet",
     name_en: "Jade Fleet",
-    name_fr: "Flotte de Jade",
   },
   "renegade-crowns": {
-    name_de: "Renegade Crowns",
     name_en: "Renegade Crowns",
-    name_fr: "Couronnes Renégates",
   },
   "slayer-host": {
-    name_de: "Slayer Host",
     name_en: "Slayer Host",
-    name_fr: "Ost Tueur",
   },
   "battle-march": {
-    name_de: "Battle March",
     name_en: "Battle March",
-    name_fr: "Colonne de Bataille",
   },
   "incantation-scrolls": {
-    name_de: "Incantation Scrolls",
     name_en: "Incantation Scrolls",
-    name_fr: "Parchemins d’Incantation",
   },
   "incantation-scroll": {
-    name_de: "Incantation Scroll",
     name_en: "Incantation Scroll",
-    name_fr: "Parchemins d’Incantation",
   },
   "warriors-of-wind-and-field": {
-    name_de: "Warriors of Wind & Field",
     name_en: "Warriors of Wind & Field",
-    name_fr: "Warriors of Wind & Field",
   },
 
   // Warhammer Fantasy 6th
