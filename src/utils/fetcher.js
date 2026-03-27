5568b8ec4fb08e5c8b1daa445a3e81a8457adb0c
Aleix Paradell Navarro <paradell.aleix@gmail.com>
2026-03-28 00:22:53 +0100

    Task/migrate to vercel (#72)

--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -1,6 +1,6 @@
 import "abortcontroller-polyfill/dist/abortcontroller-polyfill-only";
 
-const baseUrl = process.env.PUBLIC_URL || "/";
+const baseUrl = process.env.PUBLIC_URL || "";
 let controller;
 
 const abortFetch = () => {
